48ecd0dcd06cb26498bee926fccb222e40b631ca
Snail Mail <>
2025-12-30 14:09:32 +0530

    Added string formatting and escape sequences and a lot more

--- languages/cranberry/highlights.scm
+++ languages/cranberry/highlights.scm
@@ -1,25 +1,20 @@
 (comment) @comment
-
 (string) @string
+(interpolated_string_content) @string
 (number) @number
 (boolean) @boolean
-(nil) @constant
+(nil) @boolean
+(self) @type
+(print) @function
+(identifier) @variable
+(all_caps_identifier) @constant
+(escape_sequence) @string.escape
 
-[
- "[" "]"
- "{" "}"
- "(" ")"
-] @punctuation.bracket
+"$" @constant
+"\"" @string
+"'" @string
+"`" @string
 
-[
- ","
- "."
- ":"
- ";"
- "::"
-] @punctuation.delimiter
-
-; Keywords
 [
 	"let"
 	"const"
@@ -38,83 +33,17 @@
 	"out"
 	"continue"
 	"=>"
-	"constructor",
 	"in"
 ] @keyword
 
-;; Operators / common punctuation as operators
-[
-	"."
-	".."
-	"^"
-	"&&"
-	"||"
-	"=="
-	"!="
-	"<="
-	">="
-	"<"
-	">"
-	"+"
-	"-"
-	"*"
-	"/"
-	"%"
-	"="
-	"!"
-	"::"
-] @operator
+(constructor) @keyword
 
-"return" @keyword.return
+(camel_case_identifier) @type
 
-;; Declarations & names (more-specific captures first)
 (function_declaration
   (snake_case_identifier) @function)
 
-(class_definition
-  (camel_case_identifier) @type)
-
-;; Parameter names
-(parameter_list
-  (identifier) @variable)
-
-  ;; Let / const bindings and let-expression bindings
-(let_statement
-  (identifier) @variable)
-(const_statement
-  (identifier) @variable)
-(let_expression
-  (identifier) @variable)
-
-;; Member access / property names (obj.prop)
-(member_expression
-  (identifier) @property)
-
-;; Dictionary shorthand entries (key = expr)
-(dictionary_shorthand_entry
-  (identifier) @property)
-
-;; Namespace paths / imports (treat as types)
-(namespace_path
-  (camel_case_identifier) @type)
-(namespace_group_import
-  (camel_case_identifier) @type)
-
-;; Call expressions (function)
 (call_expression
-  _expression: (identifier) @function)
+  name: (identifier) @function)
 
-;; Call expressions (class object)
-(call_expression
-  _expression: (camel_case_identifier) @type)
-
-;; Generic identifiers (fallback)
-(snake_case_identifier) @variable
-(camel_case_identifier) @type
-(identifier) @variable
-
-(decorator_statement
-  "@" @punctuation
-  (identifier) @decorator
-  "(" @punctuation
-  ")" @punctuation)
+(DOT_DOT) @number
